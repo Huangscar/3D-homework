572e0f4d89c7ee3e0bf00902479e170e4b430d8a
Huangscar <1260619728@qq.com>
2018-06-05 15:57:31 +0800

    week8

---
+++ week8/Health Point1/Assets/bloodbar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bloodbar : MonoBehaviour {
+	public GameObject character;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		gameObject.transform.position = new Vector3(character.transform.position.x, character.transform.position.y + 2, character.transform.position.z);
+	}
+}
---
+++ week8/Health Point1/Assets/bloodbarIMGUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bloodbarIMGUI : MonoBehaviour {
+	private Rect big;
+
+	//public GUISkin mySkin;
+	
+	private float startBlood = 100.0f;
+
+	//public GUIStyle style;
+	// Use this for initialization
+	void Start () {
+		big = new Rect(20,20, 200, 20);
+	}
+	
+	void Update() {
+		
+		
+	}
+	// Update is called once per fra
+	void OnGUI() {
+		
+		if(Input.GetKeyUp(KeyCode.C)) {
+			if(startBlood > 0) {
+				startBlood = startBlood - 10;
+			}
+			Debug.Log(startBlood);
+		}
+		if(Input.GetKeyUp(KeyCode.X)) {
+			if(startBlood < 100.0f) {
+				startBlood = startBlood + 10;
+			}
+			Debug.Log(startBlood);
+		}
+		GUI.HorizontalScrollbar(big, 0.0f, startBlood, 0.0f, 100.0f);
+		
+	}
+	
+}
